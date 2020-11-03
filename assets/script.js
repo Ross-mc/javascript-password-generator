01cc0dc684d60c1ee43f13950735bac3370bc8ae
Ross McIntyre <rossmci90@hotmail.co.uk>
2020-11-03 15:55:51 +0000

    Created basic page, styles and click listener

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+const INITIAL = document.querySelector("#initial");
+const CRITERIA_CONTAINER = document.querySelector(".criteria_container");
+
+
+INITIAL.addEventListener("click", function(){
+    console.log('Initial button clicked')
+})
